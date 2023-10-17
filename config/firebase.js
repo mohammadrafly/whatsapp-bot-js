24a1f32679992f3abf4628704ddc7d3fc378c4dc
Mohammad Rafly <mohammadrafly19@gmail.com>
2023-10-17 15:22:31 +0700

    remove serviceAccountKey.json

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const firebase = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./.serviceAccountKey.json');
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
